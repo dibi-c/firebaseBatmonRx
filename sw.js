ea2fc509dbbf7373dfd84e958bbbe68541b13794
dibi-c <dyanbudi.db@gmail.com>
2026-01-11 20:54:19 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll(['index.html']); // Daftar file yang ingin disimpan offline
+const CACHE_NAME = 'my-pwa-cache-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Tahap Install: Menyimpan file ke cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// Tahap Fetch: Mengambil data dari cache jika offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
